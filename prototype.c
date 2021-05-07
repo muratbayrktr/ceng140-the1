efa3c98da3a5a5ef29b60b7ca7c020ed4e4d4b50
Murat <bayraktar.freelance@gmail.com>
2021-05-07 23:32:25 +0300

    prototype updated | still has bugs

--- prototype.c
+++ prototype.c
@@ -127,6 +127,47 @@ void paste_area(int image[MAXHEIGHT][MAXWIDTH], int copied_area[MAXHEIGHT*MAXWID
     return ;
 }
 
+void rotate_area(int image[MAXHEIGHT][MAXWIDTH],int area_to_be_rotated[MAXHEIGHT][MAXWIDTH],char rotation_direction, int rotation_degree,int copy_top_left_x, 
+                        int copy_top_left_y, int copy_bottom_right_x, int copy_bottom_right_y, int paste_top_left_y, int paste_bottom_right_x, int len)
+{
+    /* just a funny implementation */
+    int my_cos(int degree), my_sin(int degree);
+    int x, y, k, new_x, new_y;
+    int color;
+    /* correcting the rotation direction for the rotation matris */
+    if (rotation_direction == 'R') rotation_degree = 360 - rotation_degree;
+    printf("%d %d\n",paste_top_left_y,paste_bottom_right_x);
+    
+    k = 0;
+    for (y = copy_top_left_y; y <= copy_bottom_right_y; y++) {
+        for (x = copy_top_left_x; x <= copy_bottom_right_x; x++){
+            
+            area_to_be_rotated[y][x] = image[y][x];
+            k++;
+        }
+    }
+    k = 0;
+    for (y = copy_top_left_y; y <= copy_bottom_right_y; y++) {
+        for (x = copy_top_left_x; x <= copy_bottom_right_x; x++){
+            
+            color = area_to_be_rotated[y][x];
+                        /* rotating the point */
+            new_x = (x+(len/2))*my_cos(rotation_degree) - (y+(len/2))*my_sin(rotation_degree);
+            new_y = (x+(len/2))*my_sin(rotation_degree) + (y+(len/2))*my_cos(rotation_degree);
+            printf("old (%d,%d) --> (%d,%d) color: %d << %d\n",y,x,new_y,new_x,color,len);
+            image[new_y][new_x] = color;
+            k++;
+        }
+    }
+
+    return ;
+}
+
+int normalizer(int len, char direction) {
+    return 0;
+}
+
+
 void print_array(int image[MAXHEIGHT][MAXWIDTH])
 {
     int i, j;
@@ -156,16 +197,49 @@ void read_array(int image[MAXHEIGHT][MAXWIDTH])
     return ;
 }
 
+int my_cos(int degree)
+{
+    switch (degree)
+    {
+    case 0:
+        return 1;
+    case 90:
+        return 0;
+    case 180:
+        return -1;
+    case 270:
+        return 0;
+    }
+    return 0;
+}
+
+int my_sin(int degree)
+{
+    switch (degree)
+    {
+    case 0:
+        return 0;
+    case 90:
+        return 1;
+    case 180:
+        return 0;
+    case 270:
+        return -1;
+    }
+    return 0;
+}
 
 int main(void) 
 {
     /* function declarations */
+    int decide_color(int color_select[COORDINATE_DIMENSION], int image[MAXHEIGHT][MAXWIDTH]), my_sin(int degree), my_cos(int degree);
     void fill_image(int image[MAXHEIGHT][MAXWIDTH], int y, int x, int color);
-    int decide_color(int color_select[COORDINATE_DIMENSION], int image[MAXHEIGHT][MAXWIDTH]);
     void print_array(int array[MAXHEIGHT][MAXWIDTH]);
     void read_array(int array[MAXHEIGHT][MAXWIDTH]);
     void copy_area(int image[MAXHEIGHT][MAXWIDTH], int copied_area[], int copy_x1, int copy_y1, int copy_x2, int copy_y2);
     void paste_area(int image[MAXHEIGHT][MAXWIDTH], int copied_area[], int paste_x1, int paste_y1, int paste_x2, int paste_y2);
+    void rotate_area(int image[MAXHEIGHT][MAXWIDTH],int area_to_be_rotated[MAXHEIGHT][MAXWIDTH],char rotation_direction, int rotation_degree,int copy_top_left_x, 
+                        int copy_top_left_y, int copy_bottom_right_x, int copy_bottom_right_y, int paste_top_left_y, int paste_top_left_x, int len);
 
     /* variable declarations */
     int image[MAXHEIGHT][MAXWIDTH];
@@ -238,8 +312,27 @@ int main(void)
                 break;
             }
         case 'R':
-            {
-                /* code here */
+            {   /* local variable declarations */
+                int copy_top_left_x, copy_top_left_y, copy_bottom_right_x, copy_bottom_right_y;
+                int paste_top_left_x, paste_top_left_y, paste_bottom_right_x, paste_bottom_right_y;
+                int rotation_degree;
+                char rotation_direction;
+                /* this array is for containing the area and passing it to the rotat and paste functions */
+                int area_to_be_rotated[MAXHEIGHT][MAXWIDTH];
+
+
+                /* take degree, direction and also coordinates as in arrays */
+                scanf(" %c %d", &rotation_direction, &rotation_degree);
+                scanf(" %d %d %d %d", &copy_top_left_y,&copy_top_left_x,&copy_bottom_right_y,&copy_bottom_right_x);
+                scanf(" %d %d %d %d", &paste_top_left_y,&paste_top_left_x,&paste_bottom_right_y,&paste_bottom_right_x);
+
+                
+                /* copy area from the desired coordinates and pass it to the 1-dim array */
+                rotate_area(image, area_to_be_rotated, rotation_direction, rotation_degree, copy_top_left_x, 
+                copy_top_left_y, copy_bottom_right_x, copy_bottom_right_y, paste_top_left_y, paste_bottom_right_x,paste_bottom_right_x-paste_top_left_x);
+
+                /* rotate the area according to the degree and rotation */
+                print_array(image);
                 break;
             }
         default:
